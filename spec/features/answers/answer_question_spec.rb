a858c800014e5f2a6d1fe55c8d5a39a5dde985e7
Max Paprikas <kombise@gmail.com>
2016-04-27 20:00:14 +0500

    Update answer question feature

--- spec/features/answers/answer_question_spec.rb
+++ spec/features/answers/answer_question_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 feature 'answer on question' do
-  scenario 'user can answer on question' do
-    create(:question, title: 'How much is the fish?')
+  let(:question) { create(:question) }
 
+  scenario 'user can answer on question' do
+    question
     visit root_path
-    click_on 'How much is the fish?'
+    click_on question.title
     click_on 'Reply'
     fill_in 'Answer', with: 'Dunno'
     click_on 'Reply'
-
     expect(page).to have_content 'Dunno'
   end
 end
