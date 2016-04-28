c2c9700260e5c40e86cef48ab3c1a99d8973c77f
Max Paprikas <kombise@gmail.com>
2016-04-28 14:52:06 +0500

    Extract scenario in sign_in_spec

--- spec/features/users/sign_in_spec.rb
+++ spec/features/users/sign_in_spec.rb
@@ -3,9 +3,13 @@ require 'rails_helper'
 feature 'User sign in' do
   given(:user) { create(:user) }
 
-  scenario 'Registered user trying to sign in' do
+  scenario 'User can visit login page through root_path' do
     visit root_path
     click_on 'Sign in'
+  end
+
+  scenario 'Registered user trying to sign in' do
+    visit new_user_session_path
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
     click_button 'Log in'
