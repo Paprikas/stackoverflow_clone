7a3fee9c974c3d0cd84ddd25471e2ab5c4f187c6
Max Paprikas <kombise@gmail.com>
2016-04-29 18:05:55 +0500

    answer.accept! with tests. moved to model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,10 @@ class Answer < ApplicationRecord
   belongs_to :user
 
   validates :body, :question_id, :user_id, presence: true
+
+  def accept!
+    return unless persisted?
+    question.answers.update_all(accepted: false)
+    toggle!(:accepted)
+  end
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -6,4 +6,28 @@ RSpec.describe Answer, type: :model do
   it { should validate_presence_of :user_id }
   it { should belong_to :question }
   it { should belong_to :user }
+
+  describe 'toggle accepted' do
+    it 'should toggle' do
+      answer = create(:answer)
+      expect {
+        answer.accept!
+      }.to change{answer.accepted}.from(false).to(true)
+    end
+
+    it 'should untoggle' do
+      answer = create(:answer, accepted: true)
+      expect {
+        answer.accept!
+      }.to change{answer.accepted}.from(true).to(false)
+    end
+
+    it 'should not toggle unless persisted' do
+      answer = build(:answer)
+      expect {
+        answer.accept!
+      }.not_to change{answer.accepted}
+    end
+  end
+
 end
