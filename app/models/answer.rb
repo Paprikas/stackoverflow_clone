d1c484f262b8a3795a5332cd57e71eba85e7ea31
Max Paprikas <kombise@gmail.com>
2016-04-30 05:59:36 +0500

    Refactoring accept!. Another try

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,11 +5,11 @@ class Answer < ApplicationRecord
   validates :body, :question_id, :user_id, presence: true
 
   def accept!
+    return unless valid?
     toggle(:accepted)
 
-    if persisted? && question.present?
-      question.answers.update_all(accepted: false)
-      save
-    end
+    return unless persisted? && question.present?
+    question.answers.update_all(accepted: false)
+    save
   end
 end
