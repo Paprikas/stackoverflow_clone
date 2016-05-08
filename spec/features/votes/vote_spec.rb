d88788af528a084029ae140006eb8872fe2aeaa8
Max Paprikas <kombise@gmail.com>
2016-05-08 09:43:02 +0500

    Move vote spec to own file

--- spec/features/votes/vote_spec.rb
+++ spec/features/votes/vote_spec.rb
@@ -1,3 +1,5 @@
+require 'rails_helper'
+
 shared_examples 'votes feature' do
   context 'as user' do
     background do
@@ -57,4 +59,25 @@ shared_examples 'view vote score' do
       expect(page).to have_content 'Score 0'
     end
   end
+end
+
+feature 'vote for answer' do
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+  given!(:answer) { create(:answer, question: question) }
+
+  given(:votable) { answer }
+  given(:votable_selector) { "#answer_#{answer.id}" }
+
+  it_behaves_like 'votes feature'
+end
+
+feature 'vote for question' do
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+
+  given(:votable) { question }
+  given(:votable_selector) { "#question_#{question.id}" }
+
+  it_behaves_like 'votes feature'
 end
