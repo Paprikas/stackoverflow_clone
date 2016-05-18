6bca66ccf5b86c8ab64f481b2c4786d8d9eac054
Max Paprikas <kombise@gmail.com>
2016-05-19 00:51:31 +0500

    Do not include associations on create

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::QuestionsController < Api::V1::ApplicationController
   def create
     authorize Question
     @question = Question.create(question_params.merge(user: current_user))
-    respond_with @question
+    respond_with @question, include: ''
   end
 
   def show
