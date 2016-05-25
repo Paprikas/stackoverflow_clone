4371ee4e07856d869794363f6f4fe31bf76c8c84
Max Paprikas <kombise@gmail.com>
2016-05-26 01:38:25 +0500

    Scope spring to load in test only

--- config/initializers/spring.rb
+++ config/initializers/spring.rb
@@ -1,20 +1,22 @@
-# config/spring.rb
-require 'spring/application'
+if Rails.env.test?
+  # config/spring.rb
+  require 'spring/application'
 
-module Spring
-  class Application
-    alias connect_database_orig connect_database
+  module Spring
+    class Application
+      alias connect_database_orig connect_database
 
-    def connect_database
-      disconnect_database
-      reconfigure_database
-      connect_database_orig
-    end
+      def connect_database
+        disconnect_database
+        reconfigure_database
+        connect_database_orig
+      end
 
-    def reconfigure_database
-      return unless active_record_configured?
-      ActiveRecord::Base.configurations =
-        Rails.application.config.database_configuration
+      def reconfigure_database
+        return unless active_record_configured?
+        ActiveRecord::Base.configurations =
+          Rails.application.config.database_configuration
+      end
     end
   end
 end
