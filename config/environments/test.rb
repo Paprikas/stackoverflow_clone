d7f87e3129d73c0e0d031dd4b03362573782f663
Max Paprikas <kombise@gmail.com>
2016-06-04 16:04:42 +0500

    Fix: NameError: uninitialized constant TEST_PORT

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -43,5 +43,5 @@ Rails.application.configure do
   config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: "localhost",
-                                               port: TEST_PORT }
+                                               port: defined?(TEST_PORT) ? TEST_PORT : 3001 }
 end
