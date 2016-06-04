47f1d0201b98a31b2111c1dc0e9c93b8150423df
Max Paprikas <kombise@gmail.com>
2016-06-04 15:43:00 +0500

    Randomize port and config sphinx to work with parallel test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -43,5 +43,5 @@ Rails.application.configure do
   config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: "localhost",
-                                               port: 3001 }
+                                               port: TEST_PORT }
 end
